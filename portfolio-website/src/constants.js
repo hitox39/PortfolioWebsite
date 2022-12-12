a36bceba38f2c4c7a7694fbac9fa4d07837309a4
Chandler <93676693+hitox39@users.noreply.github.com>
2022-12-12 18:20:39 -0500

    Revert "Updates"
    This reverts commit ec9ff37b260745c6361e594c9f265bcbadc82e9a.

---
+++ portfolio-website/src/constants.js
@@ -0,0 +1,9 @@
+
+
+export const kayakBody = ' Service that allows users to find local kayakers, where they can connect with others who enjoy different aspects of being on the water and    schedule different kayak events. Users can use a discussion board to start conversations with others and learn more about the passion of being on the water. CRUD REST API with Microsoft SQL Server database, using Entity Framework for migrations.';
+
+export const movieBody = ' Movie Review service allows users to rate and review their favorite movies. CRUD REST API with Microsoft SQL Server database, using Entity Framework for migrations.';
+
+export const portfolioBody = ' My portfolio website, made with React and modern Bootstrap.';
+
+
